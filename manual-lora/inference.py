d9cd51d5f1ded9a53135b8f31117c04460281074
Stav Sapir <stavsap@gmail.com>
2024-08-08 19:04:48 +0300

    info
    Signed-off-by: Stav Sapir <stavsap@gmail.com>

--- manual-lora/inference.py
+++ manual-lora/inference.py
@@ -2,14 +2,20 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model_path = "./base"
 
-model = AutoModelForCausalLM.from_pretrained(model_path,
-                                             device_map="cuda",
-                                             trust_remote_code=False,
-                                             revision="main")
+# model = AutoModelForCausalLM.from_pretrained(model_path, device_map="cuda", trust_remote_code=False, revision="main")
 
 tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
 
-print(tokenizer.get_chat_template())
+chat = [
+   {"role": "user", "content": "Hello, how are you?"},
+   {"role": "assistant", "content": "I'm doing great. How can I help you today?"},
+   {"role": "user", "content": "I'd like to show off how chat templating works!"},
+]
+
+print(tokenizer.apply_chat_template(chat, tokenize=False))
+
+
+# print(tokenizer.get_chat_template())
 
 # model.eval()
 #
