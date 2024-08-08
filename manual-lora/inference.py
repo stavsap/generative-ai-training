014ac2c1d85ac9715de45d4e070d772b26e3aaf4
Stav Sapir <stavsap@gmail.com>
2024-08-08 16:16:52 +0300

    info
    Signed-off-by: Stav Sapir <stavsap@gmail.com>

---
+++ manual-lora/inference.py
@@ -0,0 +1,18 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_path = "./base"
+
+model = AutoModelForCausalLM.from_pretrained(model_path,
+                                             device_map="cuda",
+                                             trust_remote_code=False,
+                                             revision="main")
+
+tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
+
+model.eval()
+
+inputs = tokenizer('What is the largest animal?', return_tensors="pt")
+
+outputs = model.generate(input_ids=inputs["input_ids"].to("cuda"))
+
+print(tokenizer.batch_decode(outputs)[0])
