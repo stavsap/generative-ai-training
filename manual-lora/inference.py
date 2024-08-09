202439cfbff7e26e086b8197790a1aba18d6da1d
Stav Sapir <stavsap@gmail.com>
2024-08-09 14:34:21 +0300

    info
    Signed-off-by: Stav Sapir <stavsap@gmail.com>

--- manual-lora/inference.py
+++ manual-lora/inference.py
@@ -7,7 +7,7 @@ model = AutoModelForCausalLM.from_pretrained(model_path, device_map="cuda", trus
 tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
 
 # chat = [
-#    {"role": "user", "content": "Hello, how are you?"},
+#    {"role": "user", "content": "What is art?"},
 #    {"role": "assistant", "content": "I'm doing great. How can I help you today?"},
 #    {"role": "user", "content": "I'd like to show off how chat templating works!"},
 #    {"role": "assistant", "content": "I'm doing great. How can I help you today?"},
@@ -19,10 +19,27 @@ tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
 #
 # print(tokenizer.get_chat_template())
 
-model.eval()
+# model.eval()
 
-inputs = tokenizer("what is art?", return_tensors="pt")
+input_text = "Write me a poem about Machine Learning."
 
-outputs = model.generate(input_ids=inputs["input_ids"].to("cuda"), max_length=250)
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
 
-print(tokenizer.batch_decode(outputs)[0])
+outputs = model.generate(**input_ids, max_new_tokens=250)
+
+print(tokenizer.decode(outputs[0]))
+
+
+# import torch
+# from transformers import pipeline
+#
+# pipe = pipeline(
+#     "text-generation",
+#     model=model_path,
+#     device="cuda",  # replace with "mps" to run on a Mac device
+# )
+#
+# text = "Once upon a time,"
+# outputs = pipe(text, max_new_tokens=256)
+# response = outputs[0]["generated_text"]
+# print(response)
