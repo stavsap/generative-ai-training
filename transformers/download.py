5951e529c37be081c37bbe659af55017ba8a86ba
stav <stav.sapir@gmeil.com>
2024-08-13 20:01:38 +0300

    fixes

--- transformers/download.py
+++ transformers/download.py
@@ -5,15 +5,18 @@ token = os.getenv('HF_TOKEN')
 target_dir = 'base'
 # model_repo_id = "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ"
 # model_repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
-# model_repo_id = "meta-llama/Meta-Llama-3.1-8B"
-# model_repo_id = "google/gemma-2b"
-# model_repo_id = "google/gemma-2-2b"
+# model_repo_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+# model_repo_id = "meta-llama/Meta-Llama-3.1-70B-Instruct"
 model_repo_id = "google/gemma-2-2b-it"
+model_repo_id = "google/gemma-2-9b-it"
+model_repo_id = "google/gemma-2-27b-it"
 
 if token is "":
     token = None
 
-snapshot_download(repo_id=model_repo_id, local_dir=target_dir,
-                  local_dir_use_symlinks=False, revision="main",
+snapshot_download(repo_id=model_repo_id,
+                  local_dir=target_dir,
+                  local_dir_use_symlinks=False,
+                  revision="main",
                   token=token)
 
